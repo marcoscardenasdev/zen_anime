ba6cb4d69be1517175122441778b6db7eb4f9f12
marcoscardenasdev <marcos.cardenas.dev@gmail.com>
2026-05-24 21:12:48 -0600

    Refactor app router to improve route structure for HomeScreen and AnimeScreen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,6 +1,21 @@
 import 'package:go_router/go_router.dart';
+
 import 'package:zen_anime/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => const HomeScreen())],
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'anime/:id',
+          builder: (context, state) {
+            final id = int.parse(state.pathParameters['id']!);
+            return AnimeScreen(id: id);
+          },
+        ),
+      ],
+    ),
+  ],
 );
