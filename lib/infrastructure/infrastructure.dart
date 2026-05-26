fcf78f9790b851b990aca455c2c941439d6cca87
marcoscardenasdev <marcos.cardenas.dev@gmail.com>
2026-05-25 22:31:13 -0600

    Refactor infrastructure exports to include character and recommendation data sources and mappers

--- lib/infrastructure/infrastructure.dart
+++ lib/infrastructure/infrastructure.dart
@@ -2,3 +2,11 @@ export 'datasources/animes_datasource_impl.dart';
 export 'mappers/anime_mapper.dart';
 export 'models/anime_jikan_response.dart';
 export 'repositories/animes_repository_impl.dart';
+
+export 'datasources/recommendations_datasource_impl.dart';
+export 'repositories/recommendations_repository_impl.dart';
+export 'mappers/recommendation_mapper.dart';
+
+export 'datasources/characters_datasource_impl.dart';
+export 'repositories/characters_repository_impl.dart';
+export 'mappers/character_mapper.dart';
